916619302f31533ff044d50eb4b679c4704bc82c
orjichimaprince <orjichimaprince@gmail.com>
2022-03-13 15:07:38 -0700

    update 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,14 +13,9 @@ int main(void)
 			putchar(i % 10 + '0');
 			if (i < 9)
 			{
-																				putchar(i % 10 + '0');
-																				if (i < 9)
-																			{
-																					putchar(',');
-																					putchar(' ');
-				
-																			}
-
+				  putchar(i % 10 + '0');   													if (i < 9)
+																		   {
+																				  putchar(',');															putchar(' ');														}
 		}
 		putchar('\n');
 		return (0);
