d8914a9997161dea6084c9fbace298c71fb44cb4
orjichimaprince <orjichimaprince@gmail.com>
2022-03-21 15:04:58 -0700

    update 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * swap_int - takes values of a pointer parameter into anther
+ *   * @a: input integer a.
+ *    * @b: input integer b
+ *     * Return: no return.
+ *      */
+
+void swap_int(int *a, int *b)
+{
+	long int n = *a;
+	
+	*a = *b;
+	*b = n;
+}
