b8b52930042eabb9eb8040dac7f0e710146430b0
orjichimaprince <orjichimaprince@gmail.com>
2022-03-21 14:54:11 -0700

    update 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_FILE                                                                                                             
+#define MAIN_FILE   
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+#endif
