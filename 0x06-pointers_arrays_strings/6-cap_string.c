27aefbcdca2aa25483c1a271591b7f4a4ab72825
orjichimaprince <orjichimaprince@gmail.com>
2022-03-24 16:47:33 -0700

    update 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@
 char *cap_string(char *str)
 {
 	int index = 0;
-	
+
 	while (str[index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
@@ -32,6 +32,6 @@ char *cap_string(char *str)
 			str[index] -= 32;
 		index++;
 	}
-	
+
 	return (str);
 }
