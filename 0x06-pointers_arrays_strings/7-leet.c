c808456dec592a403c25428dc7f54588dd009d0f
orjichimaprince <orjichimaprince@gmail.com>
2022-03-24 17:06:41 -0700

    update 77

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,7 +10,7 @@ char *leet(char *str)
 {
 	int indx1 = 0, indx2;
 	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
-	
+
 	while (str[indx1])
 	{
 		for (indx2 = 0; indx2 <= 7; indx2++)
@@ -19,10 +19,10 @@ char *leet(char *str)
 					str[indx1] - 32 == leet[indx2])
 				str[indx1] = indx2 + '0';
 		}
-		
+
 		indx1++;
-	
+
 	}
-	
+
 	return (str);
 }
