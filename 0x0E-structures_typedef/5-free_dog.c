12cc7085ffb39e6a2742b176f74f2df06fde4a32
orjichimaprince <orjichimaprince@gmail.com>
2022-04-11 22:10:01 -0700

    update 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees all malloc spaces after use
+ * @d: struct pointer which used the malloc space
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		/*free the malloc created in ascending order*/
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
