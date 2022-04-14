cfab2ad9e22aa3a689c53496cc028079d5b547ad
orjichimaprince <orjichimaprince@gmail.com>
2022-04-13 21:27:51 -0700

    update 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints function pointer and string
+ * @name: pointer to string
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	/*check if name and f are not null*/
+	if (name && f)
+	{
+		/*use the function pointer*/
+		f(name);
+	}
