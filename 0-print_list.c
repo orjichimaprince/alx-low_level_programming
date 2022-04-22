0e5e8ba37c559912da7bf7237233ee3df2fa8d0b
orjichimaprince <orjichimaprince@gmail.com>
2022-04-21 19:25:28 -0700

    updated 0

---
+++ 0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements
+ * @h: pointer to a struct
+ * Return: number of elements printed
+ */
+
+size_t print_list(const list_t *h)
+{
+	/*declare variables to be used in loop and node*/
+	size_t count = 0;
+	/*go through loop and count*/
+	while (h != NULL)
+	{
+		/*key in requirement*/
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;/*h is the address of the next struct*/
+		count++;
+	}
+	return (count);
+}
