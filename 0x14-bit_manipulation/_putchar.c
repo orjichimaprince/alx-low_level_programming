6b39ea9ba82cadf20bf13abc29f5ecee23d4008c
orjichimaprince <orjichimaprince@gmail.com>
2022-04-28 21:34:39 -0700

    task 0

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,12 @@
+nclude "main.h"
+
+/**
+ * _putchar - prints a character to stdout
+ * @ch: character to be printed
+ * Return: character to stdout
+ */
+
+int _putchar(char ch)
+{
+		return (write(1, &ch, 1));
+}
